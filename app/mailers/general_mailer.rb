64077919a03ebf8971c1dd8a468112dbe526c0ac
Johannes Seikowsky <seikowsky@googlemail.com>
2017-06-15 20:40:56 +0200

    ..

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -1,7 +1,11 @@
 class GeneralMailer < ApplicationMailer
+  require "date"
   default from: "seikowsky@gmail.com"
 
   def daily_mail()
-    mail(to: "seikowsky@gmail.com", subject: "Another day avoiding Television. Day: 5")
+    start_date =  Date.new(2017,6,12)
+    difference_in_days = Date.today - start_date
+    count = difference_in_days.to_i
+    mail(to: "seikowsky@gmail.com", subject: "Another day avoiding Television. Day: #{day_count")
   end
 end
