2383247c69d97232ea1b31f053ab27ffb010d2cb
Johannes Seikowsky <seikowsky@googlemail.com>
2017-06-15 18:33:38 +0200

    ..

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -2,6 +2,6 @@ class GeneralMailer < ApplicationMailer
   default from: "seikowsky@gmail.com"
 
   def daily_mail()
-    mail(to: "seikowsky@gmail.com", subject: "Test.")
+    mail(to: "seikowsky@gmail.com", subject: "Another day avoiding Television :) | 5")
   end
 end
--- lib/tasks/mailer_tasks.rake
+++ lib/tasks/mailer_tasks.rake
@@ -1,6 +1,6 @@
 namespace :mailer_tasks do
   desc "mailer_tasks"
-  task daily_habit_mail: :environment do
-    GeneralMailer.daily_habit_mail().deliver
+  task daily_mail: :environment do
+    GeneralMailer.daily_mail().deliver
   end
 end
