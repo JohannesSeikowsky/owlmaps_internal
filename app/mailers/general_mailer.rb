71deb48ee4c046b6b0c145738c8328ab62a0bf5b
Johannes Seikowsky <seikowsky@googlemail.com>
2017-06-15 16:42:15 +0200

    ..

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -1,7 +1,7 @@
 class GeneralMailer < ApplicationMailer
   default from: "seikowsky@gmail.com"
 
-  def daily_habit()
+  def daily_mail()
     mail(to: "seikowsky@gmail.com", subject: "Test.")
   end
 end
