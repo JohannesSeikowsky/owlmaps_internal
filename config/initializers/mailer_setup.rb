64238fcaf3fc7e8bfbdfc9f7c261d202fb3167c2
Johannes Seikowsky <seikowsky@googlemail.com>
2017-06-15 16:53:07 +0200

    ..

--- config/initializers/mailer_setup.rb
+++ config/initializers/mailer_setup.rb
@@ -8,5 +8,4 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "https://omaps-internal.herokuapp.com/" if Rails.env.production?
-ActionMailer::Base.default_url_options[:host] = "http://localhost:3000/" if Rails.env.development?
+ActionMailer::Base.default_url_options[:host] = "https://omaps-internal.herokuapp.com/"
