2b7e86da110aa57b5f862eba825a58dfbde622dc
Johannes Seikowsky <seikowsky@googlemail.com>
2017-06-17 02:48:09 +0200

    ..

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -2,11 +2,24 @@ class GeneralMailer < ApplicationMailer
   require "date"
   default from: "seikowsky@gmail.com"
 
-  def daily_mail()
+  def daily_mail
     start_date =  Date.new(2017,6,12)
     difference_in_days = Date.today - start_date
     count = difference_in_days.to_i.to_s
     @subject = "Another day avoiding Television. Day: #{count}"
     mail(to: "seikowsky@gmail.com", subject: @subject)
   end
+
+  def checkin_oleg
+      mail(to: "ogiberstein@gmail.com", subject: "Rolling Checkin - OwlMaps - Oleg")
+  end
+
+  def checkin_johannes
+    mail(to: "seikowsky@gmail.com", subject: "Rolling Checkin - OwlMaps - Johannes")
+  end
+
+  def checkin_yoni
+    mail(to: "Svechinskyy@gmail.com", subject: "Rolling Checkin - OwlMaps - Yoni")
+  end
+
 end
--- lib/tasks/mailer_tasks.rake
+++ lib/tasks/mailer_tasks.rake
@@ -3,4 +3,12 @@ namespace :mailer_tasks do
   task daily_mail: :environment do
     GeneralMailer.daily_mail().deliver
   end
+
+  task owlmaps_checkins: :environment do
+    GeneralMailer.checkin_johannes.deliver
+  end
 end
+
+
+
+
