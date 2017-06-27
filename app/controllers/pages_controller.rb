41435473c4b61b7aef9781ffe7df3e5ebb20879e
Johannes Seikowsky <seikowsky@googlemail.com>
2017-06-27 23:02:03 +0200

    ..

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,35 +3,19 @@ class PagesController < ApplicationController
     redirect_to checkin_oleg_path
   end
 
-  def project_owlmaps
-    @phase = Phase.last
-    @current_phase = @phase.content
-    @milestone = Milestone.last
-    @current_milestone = @milestone.content
-    @status_oleg = Status.find(1).content
-    @status_johannes = Status.find(2).content
-    @status_yoni = Status.find(3).content
-  end
-
   def checkin_oleg
     @checkin = Checkin.new
-    @status_oleg = Status.find(1).content
-    @status_johannes = Status.find(2).content
-    @status_yoni = Status.find(3).content
+    @date = Time.now.strftime("%B %d")
   end
 
   def checkin_johannes
     @checkin = Checkin.new
-    @status_oleg = Status.find(1).content
-    @status_johannes = Status.find(2).content
-    @status_yoni = Status.find(3).content
+    @date = Time.now.strftime("%B %d")
   end
 
   def checkin_yoni
     @checkin = Checkin.new
-    @status_oleg = Status.find(1).content
-    @status_johannes = Status.find(2).content
-    @status_yoni = Status.find(3).content
+    @date = Time.now.strftime("%B %d")
   end
 
   def done
